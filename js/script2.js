8700f8ac78fe02654e93228caf28b9a8582d32a0
LuisTurizo <lfturizo@mail.uniatlantico.edu.co>
2022-05-11 17:39:35 +0000

    first commit

--- js/script2.js
+++ js/script2.js
@@ -9,18 +9,4 @@ btn.onclick = function(){
 buscar.onclick = function(){
     sidebar.classList.toggle("active");
 }
-let products = document.querySelector(".products");
-let product = document.querySelector(".pro")
-let principal = document.querySelector(".principal")
-let productos = document.querySelector('.bx-grid-alt');
 
-productos.onclick = function(){
-    products.classList.toggle("active");
-    principal.classList.toggle("active")
-
-}
-
-product.onclick = function(){
-    products.classList.toggle("active")
-    principal.classList.toggle("active")
-}
